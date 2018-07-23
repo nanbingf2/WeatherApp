a9d8a5f8ee7192551ff6e7602d339c75228912e1
nanbingf2 <2488719513@qq.com>
2018-07-23 17:14:04 +0800

    Initial commit

---
+++ weather-simple/src/main/java/com/rogercw/weather/WeatherSimpleApplication.java
@@ -0,0 +1,12 @@
+package com.rogercw.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherSimpleApplication.class, args);
+    }
+}
